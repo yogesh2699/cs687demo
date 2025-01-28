37d9de1160efcfa32703d35ffbd910fd57d4f275
YOGESH GOEL <yogesh2699@gmail.com>
2025-01-28 23:36:23 +0000

    make changes in frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import ChatBot from './components/ChatBot/ChatBot';
+
+function App() {
+  return (
+    <div className="App">
+      <ChatBot />
+    </div>
+  );
+}
+
+export default App;
